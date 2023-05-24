80a91a2e0eda6ff096ceba994a9650ad1a4af466
HaYeong <hy78733@gmail.com>
2023-05-24 22:01:12 +0900

    Feat: ProductListPage 그리드 구현
    Co-authored-by: jiye <takoyakki0609@users.noreply.github.com>

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -2,21 +2,31 @@ import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import axios from '../api/axios.js';
 import ProductItem from '../components/ProductItem.jsx';
+import { Grid } from '@mui/material';
 //'/'
 
 export default function ProductListPage() {
   const [products, setProducts] = useState([]);
+
   // async await
   useEffect(() => {
     axios.get('/products').then((response) => {
       // response = {status: number, data: {}}
-
       setProducts(response.data.products);
     });
   }, []);
   return (
     <Container maxWidth="lg">
-      {products.length > 0 && <ProductItem product={products[0]} />}
+      {products.length > 0 && (
+        <Grid container spacing={1}>
+          {products.map((product, idx) => (
+            <Grid key={idx} item xs={3}>
+              <ProductItem product={product} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Container>
   );
+  // Array.map
 }
