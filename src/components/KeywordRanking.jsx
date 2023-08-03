094036f3790a1d7a9309c21d80c7c22e5ae303cf
takoyakki0609 <kjy6478@naver.com>
2023-08-04 00:18:39 +0900

    키워드랭킹 숙제 일부

--- src/components/KeywordRanking.jsx
+++ src/components/KeywordRanking.jsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
 import rankingData from '../api/KeywordRanking.json'
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Keyword = styled('a')({
     padding:'25px 0',
@@ -11,7 +13,8 @@ const Keyword = styled('a')({
 
 const CategoryBox = styled('div')({
     display:'flex',
-    columnGap:'5px'
+    columnGap:'5px',
+    transition:'all 5s ease'
 })
 
 const Category = styled('figure')({
@@ -31,9 +34,27 @@ const BrandName = styled('figcaption')({
 const ProductImage = styled('img')({
     filter:'brightness(70%)'
 })
-
+//시간이 지나면 자동으로 다음 순위 띄워주기
 
 export default function KeywordRanking(){
+   const [rankingIdx, setRandkingIdx]=useState(0);
+   useEffect(()=>{
+    const rangkingTimer = setInterval(()=>{
+      //마지막 인덱스 변수
+      const lastIndex = rankingData.keyword ;
+      setRandkingIdx(prev => {
+        if(prev >= lastIndex) {
+          return 0
+        }return prev + 1
+      })}, 5000)
+   },[])
+
+   //rangkingIdx === idx? : activeRangking : undefinde
+   const activeRangking = {
+    Height:'113px',
+
+   }
+
     return(
       <Box style={{width:'535px'}}>
         <ol style={{position:'relative'}}>
@@ -44,15 +65,18 @@ export default function KeywordRanking(){
                   <Keyword>
                     <span style={{marginRight:'20px'}}>{idx+1}</span>
                     {keyword.keyword}
+                    {/* 해당 인덱스가 rankingIdx와 같다면 opacity:1 아니면 0
+                    Idx === rangkingIdx ? 'opacity': 1 : 'opacity':0
+                    */}
                     <span style={{position:'absolute', right:'0'}}>더보기</span>
                   </Keyword>
                   <CategoryBox> 
                     {keyword.products.map((product, idx)=>{
                       return (
-                      <Category key={idx}> 
-                        <BrandName>{product.brand_name}</BrandName>
-                        <ProductImage src={product.image}/>
-                      </Category>
+                        <Category key={idx}> 
+                          <BrandName>{product.brand_name}</BrandName>
+                          <ProductImage src={product.image}/>
+                        </Category>
                       )
                     })}
                   </CategoryBox>
